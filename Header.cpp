022da465268dfae7bbc838066acaf94b405f328f
mylescapps <MylesCapps101@gmail.com>
2025-04-24 11:58:42 -0400

    Update Header.cpp

--- Header.cpp
+++ Header.cpp
@@ -1,29 +1,20 @@
 #pragma once
-#include <string>
-#include "classes.cpp"
 #ifndef HEADER_H
 #define HEADER_H
-using namespace std;
-
-// Character offset function
-char character(char start, int offset) {
-    if (!isalpha(start)) {
-        throw invalidCharacterExcpeption();
-    }
-
-    char result = start + offset;
-
-    // Prevent case crossover and non-letter results
-    if ((islower(start) && (!islower(result) || !isalpha(result))) ||
-        (isupper(start) && (!isupper(result) || !isalpha(result)))) {
-        throw invalidRangeExpection();
-    }
-
-    return result;
-}
+#include <exception>
+using namespace std;  
 
 
+class invalidCharacterExcpeption : public exception {
+public:
+    const char* what() const noexcept override;
+};
 
+class invalidRangeExpection : public exception {
+public:
+    const char* what() const noexcept override;
+};
 
+char character(char start, int offset);
 
-#endif
+#endif 
